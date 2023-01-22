02502286e38ece388ef211d1bf2261dfedbeedfe
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2023-01-22 19:45:30 +0100

    ~ Infinite Terrain Generation
    Now the terrain generates infinitely

--- Assets/SCRIPTS/Components/Map/Map.cs
+++ Assets/SCRIPTS/Components/Map/Map.cs
@@ -1,8 +1,7 @@
-using System.Collections.Generic;
 using LSB.Shared;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 namespace LSB.Components.Core {
 	public class Map : MonoBehaviour {
@@ -26,7 +25,14 @@ namespace LSB.Components.Core {
 		[Header("Unity Fields")] 
 		[SerializeField] private GameObject PlayerPrefab;
 
+		private Transform _player;
+		private Vector3Int _playerCellPosition;
 
+		private int _currentGridHeightUp;
+		private int _currentGridHeightDown;
+		private int _currentGridWidthRight;
+		private int _currentGridWidthLeft;
+		
 		private const float _LAMP_PROBABILITY = 0.01f;
 		private const float _TOWER_PROBABILITY = 0.05f;
 		private const float _CAR_PROBABILITY = 0.10f;
@@ -38,13 +44,21 @@ namespace LSB.Components.Core {
 
 		private void Awake() {
 			_cellMap = new Props[MapWidth, MapHeight];
+
+			_currentGridHeightDown = _currentGridHeightUp = MapHeight / 2;
+			_currentGridWidthLeft = _currentGridWidthRight = MapWidth / 2;
 			
-			GenerateCellMap();
-			DrawMap();
+			generateCellMap();
+			drawMap();
 		}
 
-		private void GenerateCellMap() {
-			float[,] noiseMap = GeneratePerlinNoise();
+		private void Update() {
+			_playerCellPosition = FloorTilemap.WorldToCell(_player.position);
+			infiniteGeneration();
+		}
+
+		private void generateCellMap() {
+			float[,] noiseMap = generatePerlinNoise();
 			
 			for(int y = 0; y < MapHeight; y++)
 				for (int x = 0; x < MapHeight; x++) {
@@ -105,7 +119,7 @@ namespace LSB.Components.Core {
 				}
 		}
 
-		private void DrawMap() {
+		private void drawMap() {
 			for(int y = 0; y < MapHeight; y++)
 				for (int x = 0; x < MapWidth; x++) {
 					bool isDecoration = false, isFloor = false;
@@ -174,10 +188,79 @@ namespace LSB.Components.Core {
 					}
 				}
 			
-			SpawnPlayer();
+			spawnPlayer();
 		}
 
-		private float[,] GeneratePerlinNoise() {
+		private void infiniteGeneration() {
+			Vector3Int pos;
+			Vector3Int delete;
+			
+			#region UPPER MAP
+			
+			if (_playerCellPosition.y >= _currentGridHeightUp - 8) {
+				for (int y = 1; y < 3; y++)
+					for (int x = -_currentGridWidthLeft + 1; x <= _currentGridWidthRight + 1; x++) {
+						pos = new Vector3Int(x, _currentGridHeightUp + y, 0);
+						delete = new Vector3Int(x, -_currentGridHeightDown + y, 0);
+						
+						generateTerrain(pos, delete);
+					}
+				_currentGridHeightUp += 2;
+				_currentGridHeightDown -= 2;
+			}
+
+			#endregion
+
+			#region LOWER MAP
+			
+			if (_playerCellPosition.y <= -_currentGridHeightDown + 8) {
+				for (int y = 0; y < 2; y++)
+					for (int x = -_currentGridWidthLeft + 1; x <= _currentGridWidthRight; x++) {
+						pos = new Vector3Int(x, -_currentGridHeightDown - y, 0);
+						delete = new Vector3Int(x, _currentGridHeightUp + y, 0);
+						
+						generateTerrain(pos, delete);
+					}
+				_currentGridHeightDown += 2;
+				_currentGridHeightUp -= 2;
+			}
+				
+			#endregion
+
+			#region RIGHT MAP
+
+			if (_playerCellPosition.x >= _currentGridWidthRight - 8) {
+				for (int y = -_currentGridHeightDown + 1; y <= _currentGridHeightUp; y++)
+					for (int x = 1; x < 3; x++) {
+						pos = new Vector3Int(_currentGridWidthRight + x, y, 0);
+						delete = new Vector3Int(-_currentGridWidthLeft + x, y, 0);
+						
+						generateTerrain(pos, delete);
+					}
+				_currentGridWidthRight += 2;
+				_currentGridWidthLeft -= 2;
+			}
+
+			#endregion
+
+			#region LEFT MAP
+
+			if (_playerCellPosition.x <= -_currentGridWidthLeft + 8) {
+				for (int y = -_currentGridHeightDown + 1; y <= _currentGridHeightUp; y++)
+					for (int x = 0; x < 2; x++) {
+						pos = new Vector3Int(-_currentGridWidthLeft - x, y, 0);
+						delete = new Vector3Int(_currentGridWidthRight + x, y, 0);
+						
+						generateTerrain(pos, delete);
+					}
+				_currentGridWidthLeft += 2;
+				_currentGridWidthRight -= 2;
+			}
+
+			#endregion
+		}
+
+		private float[,] generatePerlinNoise() {
 			float[,] noiseMap = new float[MapWidth, MapHeight];
 			float xOffset = Random.Range(-100000f, 100000f);
 			float yOffset = Random.Range(-100000f, 100000f);
@@ -193,10 +276,58 @@ namespace LSB.Components.Core {
 			return noiseMap;
 		}
 
-		private void SpawnPlayer() {
-			// TODO - Determine if the player can spawn
+		private void generateTerrain(Vector3Int pos, Vector3Int deletePos) {
+			FloorTilemap.SetTile(pos, Grass);
+
+			if (DecorationTilemap.GetTile(pos) == Tree[1] || DecorationTilemap.GetTile(pos) == Lamp[1] || DecorationTilemap.GetTile(pos) == Tower[1]) {
+				FloorTilemap.SetTile(deletePos, null);
+				DecorationTilemap.SetTile(deletePos, null);
+				return;
+			}
+
+			TileBase tile = null;
+			bool isFloor = false, isDecoration = false;
+			float[,] noiseMap = generatePerlinNoise();
 			
-			Instantiate(PlayerPrefab, new Vector3(0, 0), Quaternion.identity);
+			float value = noiseMap[Random.Range(0, MapWidth), Random.Range(0, MapHeight)];
+			if (value > _TREES_PROBABILITY && value <= _DECORATION_PROBABILITY) {
+				int index = Random.Range(0, Decoration.Length);
+				isFloor = true;
+				tile = Decoration[index];
+			} else if (value > _HEN_PROBABILITY && value <= _TREES_PROBABILITY) {
+				isDecoration = true;
+				tile = Tree[0];
+				DecorationTilemap.SetTile(pos + new Vector3Int(0, 1, 0), Tree[1]);
+			} else if (value <= _LAMP_PROBABILITY) {
+				isDecoration = true;
+				tile = Lamp[0];
+				DecorationTilemap.SetTile(pos + new Vector3Int(0, 1, 0), Lamp[1]);
+			} else if (value > _LAMP_PROBABILITY && value <= _TOWER_PROBABILITY) {
+				isDecoration = true;
+				tile = Tower[0];
+				DecorationTilemap.SetTile(pos + new Vector3Int(0, 1, 0), Tower[1]);
+			} else if (value > _TOWER_PROBABILITY && value <= _CAR_PROBABILITY) {
+				isDecoration = true;
+				tile = Car;
+			} else if (value > _CAR_PROBABILITY && value <= _HEN_PROBABILITY) {
+				isDecoration = true;
+				tile = Hen;
+			}
+			
+			if(isFloor) FloorTilemap.SetTile(pos, tile);
+			else if (isDecoration) {
+				FloorTilemap.SetTile(pos, Grass);
+				DecorationTilemap.SetTile(pos, tile);
+			}
+			
+			FloorTilemap.SetTile(deletePos, null);
+			DecorationTilemap.SetTile(deletePos, null);
+		}
+
+		private void spawnPlayer() {
+			// TODO - Determine if the player can spawn
+			GameObject player = Instantiate(PlayerPrefab, new Vector3(0, 0), Quaternion.identity);
+			_player = player.transform;
 		}
 	}
 }
